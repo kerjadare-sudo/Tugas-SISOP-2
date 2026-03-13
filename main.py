5a89e07bf702109f09bc98dd5efa4460ddd340b2
Ali <kerjadare@gmail.com>
2026-03-14 00:30:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import threading
+import time
+counter = 0
+def increment():
+ global counter
+ for _ in range(100000):
+     time.sleep(0.001)
+     counter += 3
+t1 = threading.Thread(target=increment)
+t2 = threading.Thread(target=increment)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print("Nilai counter:", counter)
