318d1b10802c67f0d096d2b447a2cc50227903dc
Ali <kerjadare@gmail.com>
2026-03-14 00:35:06 +0700

    Change increment value from 3 to 1 in threading example

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def increment():
  global counter
  for _ in range(100000):
      time.sleep(0.001)
-     counter += 3
+     counter += 1
 t1 = threading.Thread(target=increment)
 t2 = threading.Thread(target=increment)
 t1.start()
